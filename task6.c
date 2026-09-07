b83fd0c155d58303a2f6dc4e8e7de7692b2a8a25
Ateeb008 <ateebabbasi008@gmail.com>
2026-09-07 14:12:27 +0500

    Create task6.c

---
+++ task6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   
+printf("Name:\t Ali");
+    printf("\nAge:\t20");
+        printf("\nGrade:\tA\n");
+            printf("\n\n\n File Path: C:Users Student");
+                 printf("\n\n\n He said: ""Hello""");
+
+
+return 0;
+
+}
