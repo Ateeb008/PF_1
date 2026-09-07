9dc3edb385d128e3d7e1dbd19fc55d7b94b1656a
Ateeb008 <ateebabbasi008@gmail.com>
2026-09-07 14:09:08 +0500

    Update task2.c

--- task2.c
+++ task2.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    printf("Enter first Number: ");
+    scanf("%d",&num1);
+        printf("Enter second Number: ");
+    scanf("%d",&num2);
+        printf("\nFirst Number: %d",num1);
+        printf("\nSecond Number: %d",num2);
+return 0;
+}
