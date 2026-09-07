b97601831739af1fd1927053350c8511b6e86e31
Ateeb008 <ateebabbasi008@gmail.com>
2026-09-07 14:06:56 +0500

    task1.c

--- README.md
+++
@@ -1,4 +0,0 @@
-### PF_1
-##This is the first file\
-This is a **Bold** text\
-this is ~~strike through~~
---
+++ task1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+   char name[20],city[20];
+    printf("Enter Your Age: ");
+    scanf("%d",&age);
+    printf("Enter Your Name: ");
+    scanf("%s",&name);
+    printf("Enter Your city name: ");
+    scanf("%s",&city);
+    printf("Name: %s",name);
+     printf("\nAge: %d",age);
+      printf("\nCity: %s",city);
+      return 0;
+
+
+}
