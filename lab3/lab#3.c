b05aa97eb45c8bd3b7ad143e33747a6292472f01
Ateeb008 <ateebabbasi008@gmail.com>
2026-09-14 14:16:52 +0500

    Update lab#3.c

--- lab3/lab#3.c
+++ lab3/lab#3.c
@@ -1,18 +1,1 @@
-#include<stdio.h>
-int main()
-{
-    int age;
-   char name,city;
-    printf("Enter Your Age");
-    scanf("%d",age);
-    printf("Enter Your Name");
-    scanf("%s",name);
-    printf("Enter Your city name");
-    scanf("%s",city);
-    printf("Name: %s",name);
-     printf("Age: %d",age);
-      printf("City: %s",city);
-      return 0;
 
-
-}
