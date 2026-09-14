71a0480eab862fba95d8cda16b4e57482d8348d7
Ateeb008 <ateebabbasi008@gmail.com>
2026-09-14 14:11:10 +0500

    Add files via upload

---
+++ lab3/lab#3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+   char name,city;
+    printf("Enter Your Age");
+    scanf("%d",age);
+    printf("Enter Your Name");
+    scanf("%s",name);
+    printf("Enter Your city name");
+    scanf("%s",city);
+    printf("Name: %s",name);
+     printf("Age: %d",age);
+      printf("City: %s",city);
+      return 0;
+
+
+}
